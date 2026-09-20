b79f5c37fcc464d6a799293e9559e5233e5236f5
anita-raut1525 <anita.raut1525@gmail.com>
2026-09-20 08:22:26 +0000

    Configure dev Jenkins server with SSM

--- terraform/modules/jenkins-ec2server/main.tf
+++ terraform/modules/jenkins-ec2server/main.tf
@@ -5,9 +5,43 @@ resource "aws_instance" "jenkins" {
   vpc_security_group_ids      = [var.security_group_id]
   associate_public_ip_address = false
 
+  iam_instance_profile = aws_iam_instance_profile.jenkins.name
+
   tags = {
     Name        = "medicare-${var.environment}-jenkins"
     Environment = var.environment
     Project     = "Medicare"
   }
 }
+
+
+resource "aws_iam_role" "jenkins_ssm" {
+  name = "medicare-${var.environment}-jenkins-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_ssm" {
+  role       = aws_iam_role.jenkins_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "jenkins" {
+  name = "medicare-${var.environment}-jenkins-profile"
+  role = aws_iam_role.jenkins_ssm.name
+}
+
